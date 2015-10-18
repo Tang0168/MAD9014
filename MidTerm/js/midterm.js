604ce863faa8e8b693dc6792cf9636e827555324
Tang0168 <Shiro@Shiro.local>
2015-10-17 20:33:36 -0400

    Final

--- MidTerm/js/midterm.js
+++ MidTerm/js/midterm.js
@@ -1,31 +1,64 @@
 var loadBtn;
+var showBtn;
 var req;
-function myFunction() {
+var myData = [];
+var count = 0;
+var count2 = 1;
+var list;
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Content Loaded");
+    loadBtn = document.getElementById("loadBtn");
+    showBtn = document.getElementById("showBtn");
+    startLoad();
+});
+function startLoad() {
+    loadBtn.addEventListener("click", function() {
+        document.getElementById("loadBtn").className = "btn disabled";
+        document.getElementById("showBtn").className = "btn enabled";
+        callJason();
+    });
+}
+function callJason() {
     req = new XMLHttpRequest();
-    req.open('GET', 'users.json');
+    req.open('GET', 'users.json', true);
     req.onreadystatechange = function () {
         if (req.readyState == 4) {
             if (req.status == 200) {
-                console.log(req.responseText);
+                myData = JSON.parse(req.responseText);
+                console.log(myData);
             }
         }
     };
     req.send(null);
-    var data = data.people[].
-    loadBtn.removeEventListener("click");
+    loadBtn.removeEventListener("click",  callJason);
+    showBtn.addEventListener("click", showNxt);
 }
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Content Loaded");
-    loadBtn = document.querySelector("#loadBtn");
-    loadBtn.addEventListener("click", myFunction()
-        );
-});
-
-/*Then change the load data button to class disabled.
-Could use an if statement to move the current data over to the old data section
-Need to look up how to display the data from json.
-
--When show first/next is clicked have the function display the data in the #output1 or 2
-display img,first name, last name and email in the main display make sure their email is clickable.
-thumbnail, first name, last name in the side display but have their whole name a link to their email
-*/
+function showNxt() {
+    showBtn.innerHTML = "Show Next";
+    document.getElementById("output1").innerHTML = "<img src=" + myData[count].image + ">" + "<h2 id='name'>" + myData[count].firstName + " " + myData[count].lastName + "</h2>" + "<a href=#>" + myData[count].email + "</a>";
+    document.getElementById("name").style.textTransform = "capitalize";
+    showBtn.removeEventListener("click", showNxt)
+    showBtn.addEventListener("click", showNew);
+}
+function showNew() {
+    if (count < 25) {
+        document.getElementById("output1").innerHTML = "<img src=" + myData[count2].image + ">" + "<h2 id='name2'>" + myData[count2].firstName + " " + myData[count2].lastName + "</h2>" + "<a href=#>" + myData[count2].email + "</a>";
+        document.getElementById('name2').style.textTransform = "capitalize";
+        if (count2 < 24) {
+            document.getElementById("output2").innerHTML += "<div><img src=" + myData[count].thumbnail + ">" + "<a href=#>" + myData[count].firstName + " " + myData[count].lastName + "</a></div>";
+            count++;
+            count2++;
+            list = document.getElementById("output2");
+            list.style.textTransform = "capitalize";
+            if (count > 0) {
+                list.removeChild(list.firstChild);
+            } else {
+                console.log("Oh boy")
+            }
+        } else {
+            console.log("Gah");
+        }
+    } else {
+        console.log("Boo...");
+    }
+}
